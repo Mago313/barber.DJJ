51821b6a7234740cfcf53da61d6085d4cc1d91ef
Магомед Мадаев <magomedmadaev@MacBook-Air-Magomed.local>
2023-12-02 00:09:06 +0300

    test5

---
+++ src/pages/DatePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainLayout from "../layouts/Mainlayout";
+
+const DatePage = () => {
+  return (
+    <MainLayout title="Время" isArrow>
+      DatePage
+    </MainLayout>
+  );
+};
+
+export default DatePage;
