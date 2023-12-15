a861dd31c5d0bea5d5819fd3305c713dfe6da194
ElPacho95 <islamadaevr6@mail.ru>
2023-12-15 19:46:46 +0300

    feat calendar

--- src/pages/DatePage.tsx
+++ src/pages/DatePage.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import MainLayout from "../layouts/Mainlayout";
 
+import styles from '../styles/pages/DatePage.module.scss'
+import { Calendar } from "../components/Calendar";
+import { formatDate, getMonthesNames } from "../utils/helpers/date";
+
+console.log(getMonthesNames());
+
+
+ 
 const DatePage = () => {
+  const [selectedDate, selectDate] = React.useState(new Date())
   return (
     <MainLayout title="Время" isArrow>
-      DatePage
+      <div className={styles.container}>
+      <div className={styles.date__container}>{formatDate(selectedDate, 'DDD DD MMM YYYY')}</div>
+        <Calendar locale="ru" selectDate={selectDate} selectedDate={selectedDate}/>
+      </div>
     </MainLayout>
   );
 };
 
 export default DatePage;
+
