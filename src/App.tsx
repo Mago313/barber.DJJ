0dfea9107fa5c4bf121cde7357be58cb0a88f945
ElPacho95 <islamadaevr6@mail.ru>
2023-12-21 23:41:37 +0300

    feat loader page

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,29 @@ import CategoriesPage from "./pages/CategoriesPage"
 import DatePage from "./pages/DatePage";
 import AdminPage from "./pages/AdminPage"
 import { State } from "./types/state";
+import LoaderPage from "./pages/LoaderPage";
 
 function App() {
-  const [state, setState] = React.useState<State>({name: '', cards: [], dateTime: '', time: '', phone: '+7 ', price: 0})
+  const [state, setState] = React.useState<State>({ name: '', cards: [], dateTime: '', time: '', phone: '+7 ', price: 0 })
   const [modalActive, setModalActive] = React.useState(false)
+  const [test, setTest] = React.useState(true)
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setTest(false)
+    },5000)
+  },[])
+
+  if(test) return <LoaderPage />
+
 
   return (
     <Routes>
-      <Route path="/" element={<FirstPage state={state} setState={setState} modalActive={modalActive} setModalActive={setModalActive}/>} />
+      <Route path="/" element={<FirstPage state={state} setState={setState} modalActive={modalActive} setModalActive={setModalActive} />} />
       <Route path="/sign-in" element={<AuthorizationPage />} />
       <Route path="/categories" element={<CategoriesPage state={state} setState={setState} modalActive={modalActive} setModalActive={setModalActive} />} />
-      <Route path="/date" element={<DatePage state={state} setState={setState}  modalActive={modalActive} setModalActive={setModalActive} />} />
-      <Route path="/admin" element={<AdminPage/>} />
+      <Route path="/date" element={<DatePage state={state} setState={setState} modalActive={modalActive} setModalActive={setModalActive} />} />
+      <Route path="/admin" element={<AdminPage />} />
     </Routes>
   );
 }
